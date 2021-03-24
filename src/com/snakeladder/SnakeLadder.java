1631198786daa377eac83f87dd05f3dc3a8b96cb
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-25 00:23:55 +0530

    ADDED: Added Repeat Till Player reach Winning Position 100

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -14,37 +14,51 @@ public class SnakeLadder {
 		// sarting position of player
 		System.out.println("starting position of player is  " + START);
 
-		// roll die get a random number
-		int dieValue = (int) ((Math.random()*100)% 6) + 1;
-		System.out.println("player roll die get random number is : " + dieValue);
+		while (PLAYER_POSITION <= 100) {
+			// roll die get a random number
+			int dieValue = (int) ((Math.random() * 100) % 6) + 1;
+			System.out.println("player roll die get random number is : " + dieValue);
 
-		// get a player status status in snake and ladder
-		int playerStatus = (int) (Math.random() * 100) % 3;
+			// get a player status status in snake and ladder
+			int playerStatus = (int) (Math.random() * 100) % 3;
 
-		// print player status
-		if (playerStatus == NOPLAY) {
-			System.out.println("player is in No Play status ");
-		}
-		if (playerStatus == LADDER) {
-			System.out.println("player is in ladder status ");
-		}
-		if (playerStatus == SNAKE) {
-			System.out.println("player is in snake status ");
+			// print player status
+			if (playerStatus == NOPLAY) {
+				System.out.println("player is in No Play status ");
+			}
+			if (playerStatus == LADDER) {
+				System.out.println("player is in ladder status ");
+			}
+			if (playerStatus == SNAKE) {
+				System.out.println("player is in snake status ");
+			}
+
+			// checking player position
+			switch (playerStatus) {
+			case NOPLAY:
+				PLAYER_POSITION = PLAYER_POSITION;
+				break;
+			case LADDER:
+				PLAYER_POSITION += dieValue;
+				break;
+			default:
+				PLAYER_POSITION -= dieValue;
+				break;
+			}
+			System.out.println("player position is : " + PLAYER_POSITION);
+
+			// player position less than 0
+			if (PLAYER_POSITION < 0) {
+				PLAYER_POSITION = 0;
+				System.out.println("player position is " + PLAYER_POSITION);
+			}
+
+			// player position reaches 100
+			if (PLAYER_POSITION == 100) {
+				System.out.println("player reaches winning position " + PLAYER_POSITION);
+				break;
+			}
 		}
 
-		// checking player position
-		switch (playerStatus) {
-		case NOPLAY:
-			PLAYER_POSITION = PLAYER_POSITION;
-			break;
-		case LADDER:
-			PLAYER_POSITION += dieValue;
-			break;
-		default:
-			PLAYER_POSITION -= dieValue;
-			break;
-		}
-		System.out.println("player position is : " + PLAYER_POSITION);
 	}
-
 }
