a27cfcaccbadcf03d1ca18839c2ccb1585a985ad
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-25 00:01:38 +0530

    ADDED: Added Player are No Play Ladder or Snake

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,12 +3,48 @@ package com.snakeladder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Problem");
-		
-		final int START=0;
 
+		// variable
+		int START = 0;
+		final int NOPLAY = 0;
+		final int LADDER = 1;
+		int SNAKE = 2;
+		int PLAYER_POSITION = 0;
+
+		// sarting position of player
 		System.out.println("starting position of player is  " + START);
-	
-		int dieValue=(int) (Math.random()  % 6)+1;
-		System.out.println("player roll die get random number is : "+dieValue);
+
+		// roll die get a random number
+		int dieValue = (int) ((Math.random()*100)% 6) + 1;
+		System.out.println("player roll die get random number is : " + dieValue);
+
+		// get a player status status in snake and ladder
+		int playerStatus = (int) (Math.random() * 100) % 3;
+
+		// print player status
+		if (playerStatus == NOPLAY) {
+			System.out.println("player is in No Play status ");
+		}
+		if (playerStatus == LADDER) {
+			System.out.println("player is in ladder status ");
+		}
+		if (playerStatus == SNAKE) {
+			System.out.println("player is in snake status ");
+		}
+
+		// checking player position
+		switch (playerStatus) {
+		case NOPLAY:
+			PLAYER_POSITION = PLAYER_POSITION;
+			break;
+		case LADDER:
+			PLAYER_POSITION += dieValue;
+			break;
+		default:
+			PLAYER_POSITION -= dieValue;
+			break;
+		}
+		System.out.println("player position is : " + PLAYER_POSITION);
 	}
+
 }
