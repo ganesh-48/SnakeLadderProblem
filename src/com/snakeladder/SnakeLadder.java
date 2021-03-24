cf3492a51e4d889b03c4bbded72badd5bec7ec7a
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-24 23:28:42 +0530

    ADDED: Added player roll die get random number

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -7,5 +7,8 @@ public class SnakeLadder {
 		final int START=0;
 
 		System.out.println("starting position of player is  " + START);
+	
+		int dieValue=(int) (Math.random()  % 6)+1;
+		System.out.println("player roll die get random number is : "+dieValue);
 	}
 }
